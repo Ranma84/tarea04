991718a31af02a525d002920233c0e8ad11b210c
AngieBayasG <148921979+AngieBayasG@users.noreply.github.com>
2023-11-16 22:39:48 -0500

    Add files via upload

---
+++ 01_calif.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 15-11-2023 a las 23:54:15
+-- Versión del servidor: 5.7.33
+-- Versión de PHP: 8.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `01_calif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignaturas`
+--
+
+CREATE TABLE `asignaturas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `obs` text,
+  `usuario_id_creacion` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT NULL,
+  `hora_creacion` time DEFAULT NULL,
+  `usuario_id_actualizacion` int(11) DEFAULT NULL,
+  `fecha_actualizacion` timestamp NULL DEFAULT NULL,
+  `hora_actualizacion` time DEFAULT NULL,
+  `usuario_id_eliminacion` int(11) DEFAULT NULL,
+  `fecha_eliminacion` timestamp NULL DEFAULT NULL,
+  `hora_eliminacion` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignaturas_estudiante`
+--
+
+CREATE TABLE `asignaturas_estudiante` (
+  `id` int(11) NOT NULL,
+  `lugar_id` int(11) DEFAULT NULL,
+  `asignatura_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL COMMENT 'Estudiante',
+  `usuario_id_creacion` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT NULL,
+  `hora_creacion` time DEFAULT NULL,
+  `usuario_id_actualizacion` int(11) DEFAULT NULL,
+  `fecha_actualizacion` timestamp NULL DEFAULT NULL,
+  `hora_actualizacion` time DEFAULT NULL,
+  `usuario_id_eliminacion` int(11) DEFAULT NULL,
+  `fecha_eliminacion` timestamp NULL DEFAULT NULL,
+  `hora_eliminacion` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lugares`
+--
+
+CREATE TABLE `lugares` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `obs` text,
+  `usuario_id_creacion` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT NULL,
+  `hora_creacion` time DEFAULT NULL,
+  `usuario_id_actualizacion` int(11) DEFAULT NULL,
+  `fecha_actualizacion` timestamp NULL DEFAULT NULL,
+  `hora_actualizacion` time DEFAULT NULL,
+  `usuario_id_eliminacion` int(11) DEFAULT NULL,
+  `fecha_eliminacion` timestamp NULL DEFAULT NULL,
+  `hora_eliminacion` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL,
+  `asignatura_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL COMMENT 'Estudiante',
+  `parcial` int(1) DEFAULT NULL COMMENT '1 1er,2 2do ,3 Mejoramiento',
+  `teoria` float(6,2) DEFAULT NULL,
+  `practica` float(6,2) DEFAULT NULL,
+  `obs` text,
+  `usuario_id_creacion` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT NULL,
+  `hora_creacion` time DEFAULT NULL,
+  `usuario_id_actualizacion` int(11) DEFAULT NULL,
+  `fecha_actualizacion` timestamp NULL DEFAULT NULL,
+  `hora_actualizacion` time DEFAULT NULL,
+  `usuario_id_eliminacion` int(11) DEFAULT NULL,
+  `fecha_eliminacion` timestamp NULL DEFAULT NULL,
+  `hora_eliminacion` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `rol` int(1) DEFAULT NULL COMMENT '1 Docente, 2 Estudiante',
+  `contrasena` varchar(100) DEFAULT NULL,
+  `obs` text,
+  `usuario_id_creacion` int(11) DEFAULT NULL,
+  `fecha_creacion` timestamp NULL DEFAULT NULL,
+  `hora_creacion` time DEFAULT NULL,
+  `usuario_id_actualizacion` int(11) DEFAULT NULL,
+  `fecha_actualizacion` timestamp NULL DEFAULT NULL,
+  `hora_actualizacion` time DEFAULT NULL,
+  `usuario_id_eliminacion` int(11) DEFAULT NULL,
+  `fecha_eliminacion` timestamp NULL DEFAULT NULL,
+  `hora_eliminacion` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `rol`, `contrasena`, `obs`, `usuario_id_creacion`, `fecha_creacion`, `hora_creacion`, `usuario_id_actualizacion`, `fecha_actualizacion`, `hora_actualizacion`, `usuario_id_eliminacion`, `fecha_eliminacion`, `hora_eliminacion`) VALUES
+(1, 'Jorge Francisco Vera Mosquera', 'joveram2010@gmail.com', 1, '18d532e9b45c72b88deec75bdc203a2b', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignaturas`
+--
+ALTER TABLE `asignaturas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `asignaturas_estudiante`
+--
+ALTER TABLE `asignaturas_estudiante`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `lugares`
+--
+ALTER TABLE `lugares`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignaturas`
+--
+ALTER TABLE `asignaturas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asignaturas_estudiante`
+--
+ALTER TABLE `asignaturas_estudiante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `lugares`
+--
+ALTER TABLE `lugares`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
