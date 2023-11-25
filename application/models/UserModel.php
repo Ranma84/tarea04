b8808ef5d0704d1fd16db8ea7ab16c16113faea2
John Quezada <john_quezada@hotmail.com>
2023-11-24 23:11:34 -0500

    Actualizacion de modelos
    Actualizacion de modelos

---
+++ application/models/UserModel.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserModel extends CI_Model {
+
+    public function getUserByUsername($username) {
+        // Obtener información del usuario por nombre de usuario
+    }
+
+    public function updateUserLastLogin($userId) {
+        // Actualizar la marca de tiempo del último inicio de sesión del usuario
+    }
+}
